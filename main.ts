a7e3da0fbc6bca037f3ef35ce0878530adf3763a
Brroooklynn <142518071+Brroooklynn@users.noreply.github.com>
2023-08-25 10:38:56 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,58 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprites.destroyAllSpritesOfKind(SpriteKind.Projectile, effects.hearts, 500)
+    music.play(music.melodyPlayable(music.magicWand), music.PlaybackMode.UntilDone)
+})
+let projectile: Sprite = null
+let mySprite = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ...........ccc..........
+    ...........cccc.........
+    .......ccc..ccccccc.....
+    .......cccccc555555cc...
+    ........ccb5555555555c..
+    .....cc..b555555555555c.
+    .....cccb55555bcc555555c
+    ......cb555555555c55d55c
+    ......b5555555555555555c
+    ...cc.b555dd5555bb1bbbc.
+    ....ccd55ddddd55bbb335c.
+    ...ccbddddddddd5bb3335c.
+    .ccccdddddddddd55b3335c.
+    cdcccdddddb55bbd553335c.
+    cddbddddddb555bb55555c..
+    cddddddddddb5555bbccc...
+    ccddddddbd55bb55cbc.....
+    .ccddddbbbdd55ccbdc.....
+    ...ccbbbcbddddccdddc....
+    .....ccccdd555dccccc....
+    ........cccccccc........
+    `, SpriteKind.Player)
+scene.setBackgroundColor(3)
+controller.moveSprite(mySprite, 100, 100)
+mySprite.setStayInScreen(true)
+info.setScore(0)
+info.startCountdown(10)
+game.onUpdateInterval(500, function () {
+    projectile = sprites.createProjectileFromSide(img`
+        . . . . . . . 6 . . . . . . . . 
+        . . . . . . 8 6 6 . . . 6 8 . . 
+        . . . e e e 8 8 6 6 . 6 7 8 . . 
+        . . e 2 2 2 2 e 8 6 6 7 6 . . . 
+        . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
+        . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
+        e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
+        e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
+        e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
+        e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
+        e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
+        e 2 2 2 2 2 2 2 4 e 2 e e c . . 
+        e e 2 e 2 2 4 2 2 e e e c . . . 
+        e e e e 2 e 2 2 e e e c . . . . 
+        e e e 2 e e c e c c c . . . . . 
+        . c c c c c c c . . . . . . . . 
+        `, 50, 0)
+    mySprite.setPosition(0, randint(0, 120))
+})
